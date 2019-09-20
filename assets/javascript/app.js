12a10f205899f080e82cfd89238e5575fba86935
dayne beronque <dayneyulber@gmail.com>
2019-09-19 19:24:07 -0500

    initial questions

---
+++ assets/javascript/app.js
@@ -0,0 +1,18 @@
+var quizQuestions = [
+    {
+        question: 'What is the largest US State?',
+        choices: ['Texas','California', 'Puerto Rico', 'Alaska'],
+        correctAnswer:'Alaska'
+    },
+
+    {
+        question: 'What is the largest country by area in the world?',
+        choices: ['US', 'Canada', 'Russia', 'China'],
+        correctAnswer:'Russia'
+    },
+
+    {
+        question: 'What is the best selling videogame of all time?',
+        choices: ['GTA V', 'Minecraft', 'Tetris', 'Super Mario Bros'],
+        correctAnswer:'Minecraft'
+    },
